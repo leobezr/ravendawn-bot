bd3004a6d62db48d5927886e6769eef91f3b7cf5
BEZR <le.bezr@gmail.com>
2022-10-11 02:16:04 +0100

    refactor: devtool

--- src/app.py
+++ src/app.py
@@ -3,9 +3,10 @@ from lib.devtools.devtools import Devtool
 import keyboard
 import cv2 as cv
 import time
+import threading
 
-SCENE = cv.imread(r".\src\__mocks__\scene\attack_position\attacking-right.jpg")
-NEEDLE = cv.imread(r".\src\target\attack-position\attacking-right.png")
+from lib.masks import Masks
+
 
 def __main__():
     print("Script initiated")
@@ -18,26 +19,34 @@ def __main__():
     if dev_mode:
         devtool.trackbar.start_up()
 
-    while True:
+    while 1:
 
-        if auto_path:
-            run_key_press_commands(game_bot)
-
-        if keyboard.read_key() == "ctrl":
-            auto_path = not auto_path
-            time.sleep(.3)
-        
         if dev_mode:
             key = cv.waitKey(1)
-            devtool.use_trackbars(SCENE)
+            # devtool.use_trackbars(STAMINA_MEDIUM)
+            scene = devtool.snapshot_toolbar()
+            devtool.find_hook(STAMINA, scene, Masks.STAMINA)
+            
 
             if key == ord("q"):
                 cv.destroyAllWindows()
                 break
+        else:
+            if auto_path:
+                run_key_press_commands(game_bot)
+
+            if keyboard.read_key() == "ctrl":
+                auto_path = not auto_path
+                print(f"Auto path is set to {auto_path}")
+                time.sleep(.3)
 
 def run_key_press_commands(game_bot = Bot()):
     if keyboard.read_key() == "space":
-        game_bot.chase_mouse()
+        mouse_chaser_thread = threading.Thread(target=game_bot.chase_mouse)
+        attack_handler_thread = threading.Thread(target=game_bot.attack)
+
+        mouse_chaser_thread.start()
+        attack_handler_thread.start()
 
 if __name__ == "__main__":
     __main__()
