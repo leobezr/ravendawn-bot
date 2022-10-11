52bf6032197dc134e9778b09f7f5fa86d2840816
BEZR <le.bezr@gmail.com>
2022-10-11 02:17:29 +0100

    chore: remove unused import

--- src/lib/core.py
+++ src/lib/core.py
@@ -1,7 +1,6 @@
-from ssl import PROTOCOL_TLS_CLIENT
 import pyautogui
 
 PROCESS_NAME = "Ravendawn -"
 
 def get_game_client():
-    return pyautogui.getWindowsWithTitle(PROCESS_NAME)[0]
+    return pyautogui.getWindowsWithTitle(PROCESS_NAME)[0] 
