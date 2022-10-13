e34d8fa6645d5fc29b5e94ae59fb3bb4fa6bb2a8
BEZR <le.bezr@gmail.com>
2022-10-13 22:52:26 +0100

    refactor: changed mask levels

--- src/lib/masks.py
+++ src/lib/masks.py
@@ -1,5 +1,5 @@
 class Masks:
 
-    TARGET_CIRCLE = 0, 163, 33, 4, 255, 255
-    STAMINA = 110, 80, 71, 144, 248, 255
+    TARGET_CIRCLE = 0, 163, 56, 9, 255, 255
+    STAMINA = 122, 104, 0, 160, 255, 160
     SPELLS = 0, 0, 0, 179, 255, 255
