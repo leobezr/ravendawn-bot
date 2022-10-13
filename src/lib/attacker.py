29ae2602be0211cd19a36deffee8498763ad81c2
BEZR <le.bezr@gmail.com>
2022-10-13 22:52:16 +0100

    refactor: improved scene check using just one scene for all item in the loop

--- src/lib/attacker.py
+++ src/lib/attacker.py
@@ -27,18 +27,20 @@ class Attacker:
         
         list.sort(key=return_priority)
 
-    def _use_spell(self, spell):
-        if vision_instance.get_target_hook(spell["target_anchor"], Masks.SPELLS, threshold=.97):
+    def _use_spell(self, spell, scene):
+        if vision_instance.get_target_hook(spell["target_anchor"], Masks.SPELLS, threshold=.9, scene=scene):
             if spell["needs_casting"]:
                 self.casting = True
                 pyautogui.sleep(1)
 
                 pyautogui.press(spell["hotkey"])
                 pyautogui.sleep(spell["needs_casting"])
+                self._log(f"Pressed {spell['hotkey']} key")
             else:
                 self.casting = False
 
                 pyautogui.press(spell["hotkey"])
+                self._log(f"Pressed {spell['hotkey']} key")
                 pyautogui.sleep(1)
     
     def _log(self, msg):
@@ -58,23 +60,27 @@ class Attacker:
     def attack(self, is_walking):
         if self.is_attacking():
             self.cast_spells(is_walking)
+        else:
+            pyautogui.press("tab")
     
     def cast_spells(self, is_walking):
+        scene = self._snapshot_toolbar()
+
         if self.has_stamina():
             for spell in self.needs_stamina:
                 self._log("Using stamina spells")
-                self._use_spell(spell)
+                self._use_spell(spell, scene)
 
         else:
             for spell in self.increase_stamina:
                 self._log("Recovering stamina")
-                self._use_spell(spell)
+                self._use_spell(spell, scene)
 
     def is_attacking(self):
-        return vision_instance.get_target_hook(TARGET_CIRCLE, Masks.TARGET_CIRCLE, threshold=.35)
+        return vision_instance.get_target_hook(TARGET_CIRCLE, Masks.TARGET_CIRCLE, threshold=.36)
 
     def has_stamina(self):
-        return vision_instance.get_target_hook(STAMINA_HOOK, Masks.STAMINA, threshold=.59, crop=self.toolbar_position)
+        return vision_instance.get_target_hook(STAMINA_HOOK, Masks.STAMINA, threshold=.49, crop=self.toolbar_position)
 
     def use_spell_increase_stamina(self):
         return
