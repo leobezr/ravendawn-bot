ee38ddf760675e45801632425d8ff512a62fa96b
BEZR <le.bezr@gmail.com>
2022-10-14 20:18:00 +0100

    refactor: moved pause function to thread

--- src/lib/threads/bot.py
+++ src/lib/threads/bot.py
@@ -1,11 +1,13 @@
 from lib.bot import Bot
+import keyboard
 import threading
 
 bot = Bot()
 
 def auto_targeting():
-    mouse_chase_thread = threading.Thread(target=bot.chase_mouse)
-    targeting_thread = threading.Thread(target=bot.attack)
+    if keyboard.read_key() == "pause":
+        mouse_chase_thread = threading.Thread(target=bot.chase_mouse)
+        targeting_thread = threading.Thread(target=bot.attack)
 
-    mouse_chase_thread.start()
-    targeting_thread.start()
+        mouse_chase_thread.start()
+        targeting_thread.start()
