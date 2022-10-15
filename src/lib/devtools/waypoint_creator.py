e597d52d75e848d448fa3f32f1d2b84c9f542747
BEZR <le.bezr@gmail.com>
2022-10-15 18:29:30 +0100

    feat: devtool module, for creating waypoints

---
+++ src/lib/devtools/waypoint_creator.py
@@ -0,0 +1,32 @@
+from lib.pointer import get_pointer, game_module
+from lib.yaml_reader import get_cavebot_waypoint, save_file
+import time
+
+class WaypointCreator:
+    waypoint_name = ""
+    waypoints = []
+
+    def __init__(self, waypoint_name):
+        self.waypoint_name = waypoint_name
+        self.waypoints = get_cavebot_waypoint(waypoint_name)
+        
+        if self.waypoints[0] is None:
+            self.remove_last()
+
+    def register(self):
+        posX = get_pointer(game_module + 0x024BB0A8, [0x40, 0xB1C])
+        posY = get_pointer(game_module + 0x024BB0A8, [0x40, 0xB20])
+        posZ = get_pointer(game_module + 0x024BB0A8, [0x40, 0xB24])
+
+        if not any(wpt == (posX, posY, posZ) for wpt in self.waypoints):
+            self.waypoints.append((posX, posY, posZ))
+            print(self.waypoints)
+            time.sleep(.1)
+
+    def remove_last(self):
+        self.waypoints.pop()
+        print(self.waypoints)
+        time.sleep(.2)
+
+    def save(self):
+        save_file(self.waypoint_name, self.waypoints)
