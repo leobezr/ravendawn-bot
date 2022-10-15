16c3aabf9addda23cede83797c25bd490f922883
BEZR <le.bezr@gmail.com>
2022-10-15 18:30:02 +0100

    refactor: removed if condition to target

--- src/lib/threads/bot.py
+++ src/lib/threads/bot.py
@@ -1,13 +1,11 @@
-from lib.bot import Bot
-import keyboard
+from lib.bot import bot_instance
 import threading
 
-bot = Bot()
+bot = bot_instance
 
 def auto_targeting():
-    if keyboard.read_key() == "pause":
-        mouse_chase_thread = threading.Thread(target=bot.chase_mouse)
-        targeting_thread = threading.Thread(target=bot.attack)
+    mouse_chase_thread = threading.Thread(target=bot.chase_mouse)
+    targeting_thread = threading.Thread(target=bot.attack)
 
-        mouse_chase_thread.start()
-        targeting_thread.start()
+    mouse_chase_thread.start()
+    targeting_thread.start()
