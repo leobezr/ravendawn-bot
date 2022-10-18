2835f5c4681ef54977cea6f89467ca9ac6d80af3
BEZR <le.bezr@gmail.com>
2022-10-18 01:48:57 +0100

    feat: new map

--- src/lib/cavebot/waypoints.py
+++ src/lib/cavebot/waypoints.py
@@ -1,5 +1,6 @@
 from lib.yaml_reader import get_cavebot_waypoint
 
 Waypoints = {
-    "ravencrest": get_cavebot_waypoint("city_walk_ravencrest")
+    "ravencrest": get_cavebot_waypoint("ravencrest"),
+    "desert_turtles": get_cavebot_waypoint("desert_turtles")
 }
