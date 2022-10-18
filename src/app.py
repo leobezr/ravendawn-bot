e36a40caa7ccd7c98798cfcc762606f9d59ed10e
BEZR <le.bezr@gmail.com>
2022-10-18 01:46:51 +0100

    feat: new app structure

--- src/app.py
+++ src/app.py
@@ -1,37 +1,42 @@
 from lib.threads.bot_chase import chase_mouse, chase_mouse_pause
-from lib.threads.gathering import auto_gathering
 from lib.devtools.devtools import Devtool
 from lib.requirement_hooks import *
 from lib.cavebot.cavebot import Cavebot
 from lib.devtools.waypoint_creator import WaypointCreator
+from lib.attacker import attacker_instance
 from lib.utils import Utils
+from bot_modes import BotMode
 import keyboard
 import cv2 as cv
+
 
 def __main__():
     Utils.log("")
     Utils.log("Bezr Bot has started!")
-    
-    dev_mode = False
-    manual_mode = True
-    waypoint_mode = False
-    script_running = 1
 
-    devtool = Devtool(dev_mode)
-    cavebot = Cavebot("ravencrest")
-    waypoint = WaypointCreator("ravencrest")
+    # BOT SETTINGS
+    BOT_MODE = BotMode.cavebot
+    #
 
-    if manual_mode:
+    devtool = Devtool(BOT_MODE == BotMode.dev)
+    waypoint = WaypointCreator("desert_turtles")
+
+    if BOT_MODE == BotMode.manual:
         Utils.log("Bot is running in Manual Mode")
 
-    if waypoint_mode:
+    elif BOT_MODE == BotMode.waypointCreator:
         Utils.log("Bot is running in Waypoint Mode")
 
-    while script_running:
-        if dev_mode:
-            devtool.get_stamina()
+    elif BOT_MODE == BotMode.cavebot:
+        cavebot = Cavebot("desert_turtles")
+        attacker_instance.standalone_attack()
+        cavebot.standalone_run()
 
-        elif waypoint_mode:
+    while 1:
+        if BOT_MODE == BotMode.dev:
+            devtool.health()
+
+        elif BOT_MODE == BotMode.waypointCreator:
             key = cv.waitKey(1)
 
             if key == ord("q"):
@@ -47,16 +52,12 @@ def __main__():
             elif keyboard.read_key() == "insert":
                 waypoint.save()
 
-        elif manual_mode:
+        elif BOT_MODE == BotMode.manual:
             if keyboard.read_key() == "space":
                 chase_mouse()
 
             if keyboard.read_key() == "pause":
                 chase_mouse_pause()
-
-        else:
-
-            cavebot.start()
 
 if __name__ == "__main__":
     __main__()
