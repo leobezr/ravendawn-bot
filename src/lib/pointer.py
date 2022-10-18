b70e66857112adebab1ceac712058ca6ae35dbea
BEZR <le.bezr@gmail.com>
2022-10-18 01:48:49 +0100

    feat: centralized pointers

--- src/lib/pointer.py
+++ src/lib/pointer.py
@@ -3,8 +3,8 @@ from pymem import *
 from pymem.process import *
 from pymem.ptypes import RemotePointer
 
-pm = Pymem("ravendawn_dx-1665687500.exe")
-game_module = module_from_name(pm.process_handle, "ravendawn_dx-1665687500.exe").lpBaseOfDll
+pm = Pymem("ravendawn_dx-1666037946.exe")
+game_module = module_from_name(pm.process_handle, "ravendawn_dx-1666037946.exe").lpBaseOfDll
 
 def get_pointer(base, offsets):
     remote_pointer = RemotePointer(pm.process_handle, base)
@@ -17,3 +17,23 @@ def get_pointer(base, offsets):
             pointer_address = remote_pointer.value + offset
 
     return pymem.Pymem.read_int(pm, pointer_address)
+
+def mana_percent():
+    return get_pointer(game_module + 0x024BB9F0, [0x0, 0x158, 0x1E0])
+
+def health_percent():
+    return get_pointer(game_module + 0x024BB9F0, [0x0, 0x158, 0x88])
+
+def position():
+    posX = get_pointer(game_module + 0x024BB0C8, [0x40, 0xB1C])
+    posY = get_pointer(game_module + 0x024BB0C8, [0x40, 0xB20])
+    posZ = get_pointer(game_module + 0x024BB0C8, [0x40, 0xB24])
+
+    return (posX, posY, posZ)
+
+def stamina_amount():
+    return get_pointer(game_module + 0x024BAF90, [0x180, 0xC04])
+
+def is_attacking():
+    target_id = get_pointer(game_module + 0x024BA568, [0xA18])
+    return target_id != 0
